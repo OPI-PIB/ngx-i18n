da5ff1c9b80b9da0188832fc62ed616e77e7a8cb
Dawid Lubowiecki <lubowiecki@gmail.com>
2024-06-25 11:31:29 +0200

    v18.0.0

---
+++ e2e/modules/visual-regression.po.ts
@@ -0,0 +1,35 @@
+import { type Page, expect } from '@playwright/test';
+
+import { Language } from '../language';
+
+interface Options {
+	language: Language;
+	timeout: number;
+	fullPage: boolean;
+}
+
+export class VisualRegression {
+	static async check(
+		page: Page,
+		name: string,
+		options: Partial<Options> = {}
+	) {
+		const defaultOptions: Options = {
+			language: Language.Pl,
+			timeout: 10_000,
+			fullPage: true,
+		};
+
+		const mergedOptions: Options = { ...defaultOptions, ...options };
+
+		await expect(page).toHaveScreenshot(
+			`${name}--${mergedOptions.language}.png`,
+			{
+				mask: [page.locator('.e2e-blackout')],
+				fullPage: mergedOptions.fullPage,
+				maxDiffPixelRatio: 0.01,
+				timeout: mergedOptions.timeout,
+			}
+		);
+	}
+}
