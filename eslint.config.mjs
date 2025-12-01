f194c77bece7835081a17b4118cac1acaa611595
Dawid Lubowiecki <lubowiecki@gmail.com>
2025-12-01 10:57:00 +0100

    v21.0.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { js, ts } from '@opi_pib/eslint-config-base';
 
-const ignores = ['dist/**', 'test-results/**'];
+const ignores = ['dist/**', 'test-results/**', 'projects/e2e-app/translations/**', 'projects/e2e-app/src/assets/**'];
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
